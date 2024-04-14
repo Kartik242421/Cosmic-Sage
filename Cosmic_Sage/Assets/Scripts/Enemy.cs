2879f5a9449fab152ef2009f7e4d1ccf741f3225
Kartik <kartik242421@gmail.com>
2024-04-14 21:00:36 +0530

    score and hit vfx added

--- Cosmic_Sage/Assets/Scripts/Enemy.cs
+++ Cosmic_Sage/Assets/Scripts/Enemy.cs
@@ -5,16 +5,44 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 2;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        
+    }
     void OnParticleCollision(GameObject other)
     {
         //Debug.Log($"{name}I'm hit! by {other.gameObject.name}");
 
+        ProcessHit();
 
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
+
+    }
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+
+        hitPoints--;
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+    void KillEnemy()
+    {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
-        
         Destroy(gameObject);
-        
     }
+
+    
 }
